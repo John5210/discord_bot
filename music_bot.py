ef161c8a21d541fd5568d5c8ce6d5445f4a95e49
john5210 <jvitali2020@my.fit.edu>
2025-03-23 21:25:05 -0400

    Added loop function, pause, and resume function

--- music_bot.py
+++ music_bot.py
@@ -16,6 +16,13 @@ intents.members = True  # Member-related events
 
 bot = commands.Bot(command_prefix="*", intents=intents)
 
+# Other setup stuff
+loop_enabled = False # Global looping default
+current_song = None # Setting current song to none for looping
+
+# Adding the ability to queue songs
+song_queue = deque()
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -26,9 +33,6 @@ if TOKEN is None:
 
 # Checking to make sure ffmpeg is installed and working
 yt_dlp.utils.bug_reports_message = lambda: ''
-
-# Adding the ability to queue songs
-song_queue = deque()
 
 def play_next(vc):
     """Plays the next song in the queue"""
@@ -52,6 +56,8 @@ async def join(ctx):
 @bot.command(name="play")
 async def play(ctx, *, query: str):
     """Adds song to the queue, or plays it if the queue is empty"""
+    global current_song
+
     if ctx.voice_client is None:
         await ctx.invoke(bot.get_command("join"))
 
@@ -73,12 +79,14 @@ async def play(ctx, *, query: str):
             return
 
     vc = ctx.voice_client
-    
+    current_song = url2  # Store song URL for looping
+
     if vc.is_playing() or vc.is_paused():
         song_queue.append((url2, title))
         await ctx.send(f"Added to queue: {title}")
     else:
-        vc.play(discord.FFmpegPCMAudio(url2, options='-vn'), after=lambda e: play_next(vc))
+        ffmpeg_options = {'options': '-vn'}
+        vc.play(discord.FFmpegPCMAudio(url2, **ffmpeg_options), after=lambda e: play_next(vc))
         await ctx.send(f"Now playing: {title}")
 
 @bot.command(name="queue")
@@ -112,5 +120,52 @@ async def leave(ctx):
         await ctx.voice_client.disconnect()
     else:
         await ctx.send("Not in a VC")
+
+@bot.command(name="pause")
+async def pause(ctx):
+    """Pauses currently playing song"""
+    vc = ctx.voice_client
+    if vc and vc.is_playing():
+        vc.pause()
+        await ctx.send("Paused current song")
+    else:
+        await ctx.send("No currently playing song")
+
+@bot.command(name="resume")
+async def resume(ctx):
+    """Resumes currently paused song"""
+    vc = ctx.voice_client
+    if vc and vc.is_paused():
+        vc.resume()
+        await ctx.send("Resumed song")
+    else:
+        await ctx.send("No song currently paused")
+
+@bot.command(name="loop")
+async def loop(ctx):
+    """Toggles looping of the current song"""
+    global loop_enabled
+    loop_enabled = not loop_enabled  # Toggle loop state
+    status = "enabled" if loop_enabled else "disabled"
+    await ctx.send(f"Replay mode: {status}")
+
+def play_next(vc):
+    """Plays the next song in the queue or loops the current one if enabled"""
+    global loop_enabled, current_song
+
+    if loop_enabled and current_song:  
+        # Recreate the audio source from URL
+        ffmpeg_options = {'options': '-vn'}
+        vc.play(discord.FFmpegPCMAudio(current_song, **ffmpeg_options), after=lambda e: play_next(vc))
+        bot.loop.create_task(vc.channel.send("Replaying current song 🔁"))
+    elif song_queue:  
+        # Play the next song in queue
+        url, title = song_queue.popleft()
+        current_song = url  # Store for looping
+        ffmpeg_options = {'options': '-vn'}
+        vc.play(discord.FFmpegPCMAudio(url, **ffmpeg_options), after=lambda e: play_next(vc))
+        bot.loop.create_task(vc.channel.send(f"Now playing: {title}"))
+    else:
+        bot.loop.create_task(vc.channel.send("Queue is empty"))
 
 bot.run(TOKEN)
